55d0d30e56705da1125724349b72aba209a12507
SDRACK <steve.derek.rackham@gmail.com>
2014-12-09 16:44:45 +0000

    another step forward - nginx failing now though

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 lock '>=3.1.0'
 # application name and the github repository
 set :application, 'workoutserver'
+default_run_options[:pty] = true
 set :repo_url, 'https://SDRACK:Steve07412rails@github.com/SDRACK/workout-azure.git' #or git@github.com:SDRACK/workoutserver.git
 
 # describe the rbenv environment we are deploying into
